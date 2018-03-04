a00eb2b8236575c672d53cb35ae7c94917c5775e
koheiokazaki <koheiokazaki@koheiokazakinoMacBook-ea.local>
2018-03-04 18:03:36 +0900

    wordpress化スタート

--- assets/js/function.js
+++ assets/js/function.js
@@ -1,1 +1,22 @@
-// alert("oo");
+jQuery(function($){
+//ここに普通のjQueryの書き方で。下の行は例
+  $("#course_btn").hover(function() {
+    $("#menu").fadeIn(300);
+  }, function() {
+    $("#menu").fadeOut(300);
+  });
+
+ //  //マウスを乗せたら発動
+ // $('div').hover(function() {
+ //
+ //   //マウスを乗せたら色が変わる
+ //   $(this).css('background', '#c00');
+ //
+ // //ここにはマウスを離したときの動作を記述
+ // }, function() {
+ //
+ //   //色指定を空欄にすれば元の色に戻る
+ //   $(this).css('background', '');
+ //
+ // });
+});
